d56b57d86cca453e743cdbbc9599a771829b307c
Bill St. Clair <billstclair@gmail.com>
2022-12-01 08:57:19 -0500

    Day01.elm

--- Day01.elm
+++ Day01.elm
@@ -14,8 +14,8 @@
 module Day01 exposing (main)
 
 import Browser
-import Html exposing (Attribute, Html, div, p, text, textarea)
-import Html.Attributes exposing (cols, rows, style, value)
+import Html exposing (Attribute, Html, a, div, p, text, textarea)
+import Html.Attributes exposing (cols, href, rows, style, target, value)
 import Html.Events exposing (onInput)
 
 
@@ -74,9 +74,61 @@ view model =
             []
         , p [] [ b "Solution: " ]
         , p [] [ text model.output ]
+        , p []
+            [ text <| "Copyright " ++ special.copyright ++ " 2022, Bill St. Clair"
+            , br
+            , a [ href "https://adventofcode.com" ]
+                [ text "Advent of Code - 2022" ]
+            , text ": "
+            , a [ href "https://adventofcode.com/2022/day/1" ]
+                [ text "Day 1" ]
+            , br
+            , text "GitHub: "
+            , let
+                url =
+                    "https://github.com/billstclair/advent-of-code-2022"
+              in
+              link url url
+            ]
         ]
+
+
+br : Html msg
+br =
+    Html.br [] []
 
 
 b : String -> Html msg
 b string =
     Html.b [] [ text string ]
+
+
+stringFromCode : Int -> String
+stringFromCode code =
+    String.fromList [ Char.fromCode code ]
+
+
+special =
+    { nbsp = stringFromCode 160 -- \u00A0
+    , copyright = stringFromCode 169 -- \u00A9
+    , biohazard = stringFromCode 9763 -- \u2623
+    , black_star = stringFromCode 10036 -- \u2734
+    , hourglass = stringFromCode 8987 -- \u231B
+    , hourglass_flowing = stringFromCode 9203 -- \u23F3
+    , checkmark = stringFromCode 10003 -- \u2713
+    , middleDot = stringFromCode 183 -- \u00B7
+    }
+
+
+link : String -> String -> Html Msg
+link label url =
+    a
+        [ href url
+        , blankTarget
+        ]
+        [ text label ]
+
+
+blankTarget : Attribute msg
+blankTarget =
+    target "_blank"
